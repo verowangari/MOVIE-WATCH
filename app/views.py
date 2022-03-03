0ac9be8de25af9f997e0298d5e93d12b9bcad620
verowangari <verowangari34@gmail.com>
2022-03-03 21:25:02 +0300

    add more categories

--- app/views.py
+++ app/views.py
@@ -1,17 +1,21 @@
 from flask import render_template
 from app import app
+from request import get_movies
 # views function
 
 
 @app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html',title=title) #Takes in the name of a template file as the first argument
-
-
+    return render_template('index.html', title = title,popular = popular_movies)
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
