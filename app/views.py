8b342c4fcd34d7e25423754d982dcb14dfbe505c
verowangari <verowangari34@gmail.com>
2022-03-03 17:51:57 +0300

    in run.py , import the app instance.  then check if the script is run directly and then use the app.run() method to launch our server.

--- app/views.py
+++ app/views.py
@@ -6,4 +6,4 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('index.html') #Takes in the name of a template file as the first argument
---
+++ run.py
@@ -0,0 +1,4 @@
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug = True) #Here we import the app instance. We then check if the script is run directly and then use the app.run() method to launch our server. 
