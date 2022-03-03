277b7b174fe7d802d59f883a7ad990b1db3a078b
verowangari <verowangari34@gmail.com>
2022-03-03 18:35:32 +0300

    create parent config class, ProdConfig and DevConfig subclasses for use in both production and development

---
+++ app/config.py
@@ -0,0 +1,24 @@
+from distutils.debug import DEBUG
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+   production configuration child class 
+    
+    Args:The parent configuration class with configuration settings
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development configuration child class
+     Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG=True
+    
