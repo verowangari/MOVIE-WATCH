138d2dd1169cd8b3ad2117fa9f2138fcd80ec5fa
verowangari <verowangari34@gmail.com>
2022-03-03 22:08:15 +0300

    in the movie html, create a template with two columns for passing in the movie poster and details

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies,get_movie
 
 
 
@@ -28,3 +28,13 @@ def movie(movie_id):
     '''
     title = f'You are viewing {movie_id}'
     return render_template('movie.html',title = title)
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
