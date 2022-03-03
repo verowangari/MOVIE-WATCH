e8b7d8aba3ffbef7123b5912931da4983c9e5314
verowangari <verowangari34@gmail.com>
2022-03-03 21:58:07 +0300

    add twitter bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
 #Initializing applicator
 app=Flask(__name__,instance_relative_config=True)
 
 #Setting up Configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from app import app
-from request import get_movies
-# views function
+from .request import get_movies
 
 
+
+# Views
 @app.route('/')
 def index():
 
@@ -13,13 +14,17 @@ def index():
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
-@app.route('/movie/<int:movie_id>')
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+
+@app.route('/movie/<movie_id>')
 def movie(movie_id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title)
