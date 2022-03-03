9e2eada79d455b308d0acfa5d312b4f75935b9cc
verowangari <verowangari34@gmail.com>
2022-03-03 18:29:06 +0300

    route movie html file and convert id to interger

--- app/views.py
+++ app/views.py
@@ -6,4 +6,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html') #Takes in the name of a template file as the first argument
+    message='I am ambitious'
+    return render_template('index.html',message=message) #Takes in the name of a template file as the first argument
+
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
