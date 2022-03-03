4006f8f585f11b5c4ad4546ae82fa2c30e870aac
verowangari <verowangari34@gmail.com>
2022-03-03 19:26:55 +0300

    import urlibrequest and json

---
+++ app/movie_test.py
@@ -0,0 +1,17 @@
+import unittest
+from models import movie
+Movie=movie.Movie
+
+class Movietest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the Movie Class
+    '''
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+if __name__ == '__main__':
+    unittest.main()
--- app/request.py
+++ app/request.py
@@ -1,4 +1,13 @@
 from app import app
+import urllib.request,json
+from .models import movie
+
+Movie =movie.Movie
 
 #Getting api key
 api_key = app.config['MOVIE_API_KEY']
+
+#Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
+
+#accessing our app configuration objects
